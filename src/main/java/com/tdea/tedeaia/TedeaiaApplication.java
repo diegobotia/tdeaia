7c62bbb5314c169a21ff142c05cf4747eeacd0ba
DB <diego.botia@udea.edu.co>
2026-05-20 18:06:19 -0500

    first commit

---
+++ src/main/java/com/tdea/tedeaia/TedeaiaApplication.java
@@ -0,0 +1,13 @@
+package com.tdea.tedeaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TedeaiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TedeaiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdea/tedeaia/TedeaiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdea.tedeaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TedeaiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
